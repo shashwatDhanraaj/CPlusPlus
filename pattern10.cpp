29bdcd636016b26f375c0fd7a1cd70081ec45d36
sdhanraaj12 <sdhanraaj12@gmail.com>
2022-12-16 19:53:41 +0530

    ci

---
+++ pattern10.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    int row = 1;
+
+    while (row<=n)
+    {
+        //space
+        
+        int space = row-1;
+        while (space)
+        {
+            cout<<" ";
+            space--;
+        }
+        //star
+        int col = 1;
+        while (col<=n-row+1)
+        {
+            cout<<"*";
+            col++;
+        }
+        cout<<endl;
+        row++;
+        
+        
+
+    }
+    
+}
